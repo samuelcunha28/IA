a91b069515be1dc22c59e4cfc4bf11c746fb52f8
Samuel Cunha <samuellucianocorreiacunha@hotmail.com>
2021-04-18 12:44:02 +0100

    update

--- FP 04/swish.pl
+++ FP 04/swish.pl
@@ -32,6 +32,9 @@ copia(1,2).
 copia(2,3).
 copia(3,4).
 
+%achar todas as notas de um dado aluno
+student_list(Aluno, Lista):- findall(Nota, nota(Aluno,_,Nota), Lista). 
+
 %Quais os alunos que não estão inscritos em qualquer disciplina
 %findall(X,(aluno(X,_,_), not(inscrito(X,_))), L).
 
@@ -51,6 +54,33 @@ naoInscritos(NaoInscritos):-
 %Qual a media de um determinado aluno
 media_aluno(X):- aluno(X,_,_), nota(X,_,_), T1 is T+N, C1 is C+1, Y is T1/C1.
 
+
+
+average( Aluno, Average ):- student_list(Aluno, List),
+                            list_sum( List, Sum ),
+                            length( List, Length ),
+                            Length > 0,
+                            Average is Sum / Length.
+
+%media de todos alunos : average( X,Average)
+%media de um determinado aluno : average( X,Average) , onde X ]e o numero de aluno
+
+%Get all unique student values 
+unique(Result) :- std_em_nota(List), sort(List, Result ).
+
+totalAvg(Result):- average( _, Result ).
+
+%Alunos cujo as medias sao acima da media total
+%query: students_avg(Average)
+students_avg(Average):- unique(Result),
+    					member(Resultado, Result),
+    					average( Resultado, Average ),
+    					totalAvg(TotalAvg),
+    					Average > TotalAvg.
+
+
+
+
 %Quais os alunos cuja média é acima da média (considere todas as notas de todas as disciplinas)
 alunoAcimaMedia(X):-
     findall(X, (aluno(X,_,_), nota(X,_,_)), Alunos),
@@ -67,18 +97,73 @@ copiouPor(X,Y):- copia(X,Z), copiouPor(Z,Y).
 %mapToNome - converter uma lista de números de alunos numa lista de nomes. Assuma que podem ser dados números de
 %alunos não registados (que devem ser ignorados)
 mapToNome([H|T]):- aluno(H,Y,_), mapToNome[T].
+lista_de_alunos(Lista, AlunoName):- member(AlunoNumber, Lista),aluno(AlunoNumber, AlunoName,_).
 
 
-posicao(X,Y,Inimigo).
-eu(X,Y).
-accuracy(Inimigo, P).
-disparo(Inimigo, Acertou).
+%posicao: posicao(X,Y,Inimigo).
+posicao(10, 10, robot1).
+posicao(10, 5, robot2).
 
-%calcula a distancia a que um dado inimigo se encontra do meu robot
-distancia(X,Y).
+%minha posicao: eu(X,Y).
+eu(0,0).
+%precisao: accuracy(Inimigo, P).
+accuracy(robot1, 50).
+accuracy(robot2, 70).
+%disparo da bala: disparo(Inimigo, Acertou).
+disparo(robot1, hit).
 
-%calcula a distancia média a que o meu robot se encontra dos inimigos
-distanciaMedia(X).
+%calcula a distancia a que um dado inimigo se encontra do meu robot: distancia(X,Y).
+distancia(Inimigo, D):-
+    posicao(X1,Y1,Inimigo),
+    eu(X2,Y2),
+    D is sqrt((X2-X1)^2 + (Y2-Y1)^2).
 
-%calcula o total de inimigos com os quais estou a combater
-totalInimigos(X).
+%lista de distancias
+distance_list(ResultList):- findall(D, distance(_, D), ResultList).
+
+%calcula a distancia média a que o meu robot se encontra dos inimigos: distanciaMedia(X).
+avg_distance(Average):- distance_list(ResultList),
+    					list_sum( ResultList, Sum ),
+    					length( ResultList, Length ), 
+    					Length > 0,
+                        Average is Sum / Length.
+
+%calcula o total de inimigos com os quais estou a combater: totalInimigos(X).
+totalInimigos(Count):- aggregate_all(count, posicao(_,_,_), Count).
+
+
+%Find minimum value in list
+min_in_list([Min],Min).                 % We've found the minimum
+
+min_in_list([H,K|T],M) :- H =< K,                             % H is less than or equal to K
+                          min_in_list([H|T],M).               % so use H
+
+min_in_list([H,K|T],M) :- H > K,                              % H is greater than K
+    					  min_in_list([K|T],M). 
+%Find minimum value in list
+
+%Achar enimigo mais perto
+find_closest_enemy(Min):-distance_list(ResultList), min_in_list(ResultList,Min).
+
+%Lista de enimigos cujas eficacias sao superiores a 75%
+
+%obter todos os nomes dos enimigos cujas as eficacias superiores a 75%
+medias_sup(NameResult):- accuracy(_, P), P> 75, accuracy(NameResult, P).
+
+%criar lista de eficacias
+max_eff_targets(ResultList):- findall(P, medias_sup(P), ResultList).
+%query max_eff_targets(ResultList)
+
+
+
+disparar(Enemy, Potencia):- Enemy == sitingDuck, Potencia is 3.
+disparar(Enemy, Potencia):- accuracy(Enemy, Accuracy),
+    						Accuracy > 75,
+    						distance(Enemy, D),
+    						D< 100,
+    						Potencia is 3.
+disparar(Enemy, Potencia):- velocity(Enemy, V), V > 4, Potencia is 0.
+disparar(Enemy, Potencia):- velocity(Enemy, V), V < -4, Potencia is 0.
+
+%Se a velocidade estiver entre 1 e 2, usar isto
+range(X) :- X>=1, X =<2.
